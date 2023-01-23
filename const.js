2bb697ffda59fd86c361c578d49a911532f117e7
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-23 12:15:47 +0500

    String Indexing

---
+++ const.js
@@ -0,0 +1,24 @@
+// Declare Constant
+
+var Question1 = 'What Is Contant';
+
+console.log(Question1);
+
+var Answer1 = "Contant is Simple which is not change Example Below";
+
+console.log(Answer1);
+
+var Example1 = "Pi(3.14) It will Not Change"
+
+console.log(Example1);
+
+var Example2 = "Avagadro's Number (6.022 × 10²³)";
+
+console.log(Example2);
+
+const pi = 3.14;
+
+// pi = 3.15;//Uncaught TypeError: Assignment to constant variable.at const.js:21:4
+
+console.log(pi); 
+    
---
+++ string-indexing.js
@@ -0,0 +1,12 @@
+// string Indexing 
+ let firstName = "Saad Ahmed";
+
+ // S a a d space A h m e d
+ // 0 1 2 3 4     5 6 7 8 9 
+
+ console.log(firstName[9]);// String indexing 
+
+
+// Length Of String
+
+console.log(firstName.length);
