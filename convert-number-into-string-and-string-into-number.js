be580a4a45156d35d928372da811474e0de009c4
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-23 14:11:06 +0500

    Convert String into number and number into string

---
+++ convert-number-into-string-and-string-into-number.js
@@ -0,0 +1,53 @@
+// typeof Operator
+
+// data types (Primitive data tyypes)
+
+// string "Harshit"
+
+// number 2, 4, 5.6
+
+// booleans
+
+// undefined
+
+// null
+
+// BigInt
+
+// Symbol
+
+let age = 22;
+
+console.log(typeof age);
+
+let firstName = "harshit";
+console.log(typeof firstName);
+
+// console.log(typeof 's') // String
+
+
+//convert Number to string.
+// 22 -> "22"
+age = age + ""; 
+console.log(typeof(age)); //string
+
+
+//conver string to number.
+
+let myStr = +"34";
+console.log(typeof myStr);
+
+
+// Another Way
+
+let age2 = 18;
+console.log(typeof age2);
+
+age2 = String(age2);
+console.log(typeof age2);
+
+let StringName = 'saad';
+console.log(typeof StringName);
+
+StringName = Number(StringName);
+console.log(typeof StringName);
