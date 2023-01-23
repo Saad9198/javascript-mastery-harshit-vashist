01925eb9b810f5a0dd469192b1243ec19536932b
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-23 13:46:36 +0500

    Useful string Method

---
+++ useful-string-method.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+let firstName = "   Harshit    ";
+
+console.log(firstName.length);
+// trim()
+firstName = firstName.trim(); // Harshit
+console.log(firstName);
+console.log(firstName.length);
+// toUpperCase
+firstName = firstName.toUpperCase();
+console.log(firstName);
+// toLowerCase
+firstName = firstName.toLowerCase();
+console.log(firstName);
+// slice
+firstName = firstName.slice(7);
+console.log(firstName);
