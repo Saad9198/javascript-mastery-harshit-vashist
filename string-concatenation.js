6f97cb306c8412359fa3e42a553252901cc51a4a
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-23 16:47:04 +0500

    Modified Files

---
+++ string-concatenation.js
@@ -0,0 +1,20 @@
+// string Concatenation(Journa) (bind)
+
+let string1 = "Saad"; 
+let string2 = "Ahmed"; 
+
+let fullName = string1 + " " + string2;
+console.log(fullName);
+
+
+let String3 = "10";
+let String4 = "17";
+
+let TotalSum = String3 + String3 + String4;
+console.log(TotalSum);//101017
+
+let String5 = "10";
+let String6 = "17";
+
+let TotalSumString = +String5 + +String5 + +String6;
+console.log(typeof(TotalSumString));//37
---
+++ template-string.js
@@ -0,0 +1,13 @@
+// Template String
+let Age = 20;
+
+let myName = "saad"
+
+let AboutMe = "my Name is " + myName + " and my Age is " + Age;
+
+console.log(AboutMe);
+
+// Another way of declartion The Given values in the new String
+
+let AnotherMethod = `This Is Another Method My Name Is ${myName} and my Age is ${Age}`;
+console.log(AnotherMethod);
