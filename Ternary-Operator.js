00850a0a4f4d0bc581ed998fd83bb045a9a003e4
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-24 12:11:42 +0500

    Ternary Operator

---
+++ Ternary-Operator.js
@@ -0,0 +1,18 @@
+// Ternary Operator
+
+let age = 15;
+let drink;
+
+if (age >=5) {
+    drink = "coffe"
+}
+else{
+    drink = "milk"
+}
+console.log(drink);
+
+// Ternary Operator / Conditional Operator
+
+let age2 = 3;
+let drink2 = age2 >=5 ? "coffe" : "milk";
+console.log(drink2);
