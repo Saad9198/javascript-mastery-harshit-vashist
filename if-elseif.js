8b9e01e4742eed5aad0c16dd57fc2421496100f8
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-25 17:44:08 +0500

    if elseif else

--- if-elseif.js
+++ if-elseif.js
@@ -3,3 +3,46 @@
 // else if
 // else if
 // else
+
+let tempInDegree = 16;
+
+if (tempInDegree < 0) {
+    console.log("Extremely Cold Outside");
+}
+else if (tempInDegree < 16) {
+    console.log("It Is Cold Outside");
+}
+else if(tempInDegree < 25) {
+    console.log("Whether Is Okay");
+}
+else if(tempInDegree < 35) {
+    console.log("Let's Go For Swim");
+}
+else if(tempInDegree < 45) {
+    console.log("Turn On AC");
+}
+else{
+    console.log("too hot!!")
+}
+
+console.log("Hellow World");
+
+
+let TempInDegree2 = 4;
+
+if (TempInDegree2 > 40) {
+    console.log("too Hot!!");
+}
+else if (TempInDegree2 > 30) {
+    console.log("Let's Go For Swim");
+} 
+else if (TempInDegree2 > 20) {
+    console.log("Whether Is Cool");
+}
+else if(TempInDegree2 > 10) {
+    console.log("It Is Very Cold Outside");
+}
+else {
+    console.log("Extremely Cold");
+}
+console.log("Only One condition WIll Be true"); 
