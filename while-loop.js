dd7cac75594e831e707cb3fe81d61e85350c18f7
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-26 13:56:59 +0500

    While Loop

---
+++ while-loop.js
@@ -0,0 +1,44 @@
+// while loop
+
+// 0 se 9
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// This is also way but its not good practice
+
+// let i = 0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// we have a principal in Ordering(Auding) dry(Don't Repeat Yourself)
+
+let i = 0; // 0 1 2 3
+while(i<=9) {
+    console.log(i);
+    i++;
+}
+console.log(`current value of i is ${i}`);
+console.log("hellow");
