7e5208ef8567efe6ada252e705461cb078a20ebd
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-27 18:43:24 +0500

    Intro to ARray And SOme Important methods usiing In Array Like Push Pop SHift Unshift

---
+++ Arrays in JavaScript/intro-to-array.js
@@ -0,0 +1,50 @@
+// intro to arrays
+
+// how to create arrays
+
+
+var Question = "what is array";
+
+console.log(Question);
+
+var Answer = "Array Is A Reference Type.";
+
+console.log(Answer);
+
+var Question2 = "What is Reference Type?";
+
+console.log(Question2);
+
+var Answer2 = "Reference type is object and Array is a object in Javascript"
+
+var ArrayDefinition = "The Definition Of Array is Collection Type."
+
+console.log(ArrayDefinition);
+
+var TechDefinition = "In Programming and technical Terms We can define Arrays As A Ordered Collection Of Items"
+
+console.log(TechDefinition);
+
+let fruits = ["apple","mango","grapes"];
+
+console.log(fruits);
+
+let number = [1,2,3,4];
+
+console.log(number);
+
+console.log(fruits[0,1,2]); // Grapes 
+// Always show which is last when multiple indexing called
+
+let mixed = [1,2,2.3, "string", null, undefined]
+console.log(mixed);
+
+let name12 = ["saad","raees","amin"];
+name12[1] = "";
+console.log(typeof name12); //object
+
+let obj = {}; // Literal Object
+console.log(typeof obj);
+console.log(Array.isArray(name12)) // True means It Is Array
+console.log(Array.isArray(obj)) // false means it is not Array
+// array Indexing
---
+++ Arrays in JavaScript/push-pop-shift-unshift.js
@@ -0,0 +1,32 @@
+// array push pop shift unshift
+
+// important methods of Array
+
+let fruits = ["apple", "mango","Banana"];
+console.log(fruits);
+let push = "Push is adding The Value in the arrays in last Belows Example";
+console.log(push);
+
+fruits.push("grapes");
+console.log(fruits);
+// Pop
+let pop = "Pop Is removing the value from the last Example Below";
+console.log(pop);
+
+let poppedFruit = fruits.pop();
+console.log(fruits);
+console.log("popped Fruit is", poppedFruit)
+
+//  Unshift
+let unshift = "Unshift Adding The Value in the Start Of the Array Example Below";
+console.log(unshift);
+fruits.unshift("Orange");
+console.log(fruits);
+
+let shift = "Shift Is Removing the Value in start of the Arrays Example Below";
+console.log(shift);
+let ShiftFruits = fruits.shift();
+console.log(fruits);
+console.log("Shift Fruits is",ShiftFruits)
+
+
