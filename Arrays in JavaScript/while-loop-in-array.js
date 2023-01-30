ed3ff53946180ff9c90f35ce690305f3e4beb20c
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-30 16:03:36 +0500

    While Loop In Array

---
+++ Arrays in JavaScript/while-loop-in-array.js
@@ -0,0 +1,12 @@
+// while loop in array
+
+const fruits = ["apple", "mango","grapes"];
+const fruits2 =[]
+let i = 0;
+while(i<fruits.length){
+    // console.log(fruits[i]);
+    // console.log(fruits[i].toUpperCase());
+    fruits2.push(fruits[i].toUpperCase());
+    i++;
+}
+console.log(fruits2)
