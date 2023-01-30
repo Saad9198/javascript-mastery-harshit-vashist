15f7b45616b93b5c49e8a7f47c261d6ba9ad61b8
Saad Ahmed <saamed9198@sbtjapan.com>
2023-01-30 16:10:23 +0500

    For Of Loop In Array

---
+++ Arrays in JavaScript/for-of-loop-in-array.js
@@ -0,0 +1,12 @@
+//for of loop In array
+
+
+
+const fruits =["apple","mango","grapes","fruit4","fruit5"];
+const fruits2 =[];
+for (let fruit of fruits){
+    console.log(fruit);
+    console.log(fruit.toUpperCase());
+    fruits2.push(fruit.toUpperCase());
+}
+console.log(fruits2)
