fade25e5d288ac03570be970bbc51b8d9d2c5ca8
Saad Ahmed <saamed9198@sbtjapan.com>
2023-02-02 12:54:16 +0500

    Itearte Objects

--- Objects In Javascript/iterate-objects.js
+++ Objects In Javascript/iterate-objects.js
@@ -1,1 +1,22 @@
 //  How to Iterate Objects
+const person = {
+    name:"saad",
+    age:20,
+    "person hobbies":["coding","music","office"],
+}
+for (let key in person){
+    // console.log(key)  
+    // give the name of the keys 
+    // name 
+    // age 
+    // person hobbies
+    // console.log(person.key);
+    // console.log(person[key]);
+    // console.log(`${key} : ${person[key]}`);
+    // console.log(key," : ", person[key]);
+}
+console.log(Object.keys(person));
+
+for(let key of Object.keys(person)){
+    console.log(person[key])
+}
