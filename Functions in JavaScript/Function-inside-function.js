4aba29fc1bb9baf9905f952a1f652eed9ec710e1
Saad Ahmed <saamed9198@sbtjapan.com>
2023-02-23 15:59:30 +0500

    Function Inside Functio

--- Functions in JavaScript/Function-inside-function.js
+++ Functions in JavaScript/Function-inside-function.js
@@ -1,1 +1,17 @@
 //Function inside function
+
+const app = () =>{
+    console.log("inside app")
+    const myFunction = () =>{
+        console.log("I am function insdie app function")
+    }
+    myFunction();
+    const addTwo = (num1, num2) =>{
+        return num1 + num2
+    }
+    console.log(addTwo(5, 6));
+    const Mul = (num1,num2)=>num1 * num2;
+    
+    console.log(Mul(5, 6));
+}
+app();
